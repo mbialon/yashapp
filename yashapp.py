6be74c1bb5f50d7786287b710f54ddf8496b79c1
Marcin <marcin.bialon@gmail.com>
2009-03-31 21:39:20 +0200

    js function executes ajax call and prints formatting result

--- yashapp.py
+++ yashapp.py
@@ -7,17 +7,37 @@ class MainPage(webapp.RequestHandler):
   def get(self):
     self.response.out.write("""
       <html>
+      <head>
+        <script src="http://www.google.com/jsapi"></script>
+        <script>
+          google.load("jquery", "1.3");
+          google.setOnLoadCallback(function() {
+            $("input#execute_format").click(function() {
+              var snippet = $("#snippet").val();
+              $.post("/format",
+                { snippet: snippet },
+                function(data) {
+                  $("#formatted_snippet").text(data);
+                });
+              return false;
+            });
+          });
+        </script>
       <body>
         <form action="/format" method="post">
-          <div><textarea name="snippet" rows="24" cols="80"></textarea></div>
-          <div><input type="submit" value="Format"></div>
+          <div><textarea id="snippet" name="snippet" rows="10" cols="80"></textarea></div>
+          <div><input id="execute_format" type="submit" value="Format"></div>
+          <div><textarea id="formatted_snippet" rows="10" cols="80"></textarea></div>
         </form>
       </body>
       </html>""")
 
 class Format(webapp.RequestHandler):
   def post(self):
-    self.response.out.write(self.request.get('snippet'))
+    snippet = self.request.get('snippet')
+    formatted_snippet = cgi.escape(snippet)
+    self.response.out.write(formatted_snippet)
+    self.response.out.write("foobar")
 
 app = webapp.WSGIApplication([('/', MainPage), ('/format', Format)], debug=True)
 
