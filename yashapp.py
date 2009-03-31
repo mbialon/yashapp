5c79dab0cdac797f75e348f3c4bb8589ee66024f
Marcin <marcin.bialon@gmail.com>
2009-03-31 21:20:22 +0200

    main page send code snippet to format action

--- yashapp.py
+++ yashapp.py
@@ -1,3 +1,28 @@
-print 'Content-Type: text/plain'
-print ''
-print 'Hello, world!'
+import cgi
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp.RequestHandler):
+  def get(self):
+    self.response.out.write("""
+      <html>
+      <body>
+        <form action="/format" method="post">
+          <div><textarea name="snippet" rows="24" cols="80"></textarea></div>
+          <div><input type="submit" value="Format"></div>
+        </form>
+      </body>
+      </html>""")
+
+class Format(webapp.RequestHandler):
+  def post(self):
+    self.response.out.write(self.request.get('snippet'))
+
+app = webapp.WSGIApplication([('/', MainPage), ('/format', Format)], debug=True)
+
+def main():
+  run_wsgi_app(app)
+
+if __name__ == "__main__":
+  main()
